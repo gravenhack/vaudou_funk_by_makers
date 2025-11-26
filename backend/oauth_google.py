c51c7394931c107238f11c7fbef4f1e1f5f4599d
Audry Musk <audry.sahossi@epitech.eu>
2025-11-26 12:49:13 +0100

    fix:select google account fixed

--- backend/oauth_google.py
+++ backend/oauth_google.py
@@ -17,6 +17,7 @@ oauth.register(
     client_secret=os.getenv('GOOGLE_CLIENT_SECRET'),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={
-        'scope': 'openid email profile'
+        'scope': 'openid email profile',
+        'prompt': 'select_account'
     }
 )
